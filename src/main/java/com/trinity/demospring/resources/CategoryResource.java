0cd513769b25c45c98f1ed8ff6e1482c3ae4a78c
patrickpr86 <sh4dbr@gmail.com>
2020-07-28 12:38:07 -0300

    Creating  entity Category and testing

--- src/main/java/com/trinity/demospring/resources/CategoryResource.java
+++ src/main/java/com/trinity/demospring/resources/CategoryResource.java
@@ -1,16 +1,29 @@
 package com.trinity.demospring.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.trinity.demospring.entities.Category;
+
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
-	
+
 	@GetMapping
-	public String list() {
-		return "rest on";
+	public List<Category> list() {
+		Category cat = new Category(null, "TI");
+		Category cat2 = new Category(null, "Office");
+
+		List<Category> list = new ArrayList<>();
+		list.add(cat);
+		list.add(cat2);
+
+		return list;
+
 	}
 
 }
