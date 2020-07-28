8db34fc7b1e788d3c677130ae5660f34086d8a48
patrickpr86 <sh4dbr@gmail.com>
2020-07-28 12:20:15 -0300

    Tested REST

---
+++ src/main/java/com/trinity/demospring/resources/CategoryResource.java
@@ -0,0 +1,16 @@
+package com.trinity.demospring.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+	
+	@GetMapping
+	public String list() {
+		return "rest on";
+	}
+
+}
